19a5117aa241b5e8aa9f41fa294fc0697c458c69
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-26 23:30:21 -0500

    Added getFile method to FileService

--- functions/src/service/file.service.ts
+++ functions/src/service/file.service.ts
@@ -18,4 +18,25 @@ export class FileService {
             }
         )
     }
+    public async getFile(filename) {
+        return new Promise<any>(
+            async (resolve, reject) => {
+                const path: string = `uploads/${filename}`;
+                const download = this.storage.bucket().file(path);
+                const readStream = download.createReadStream();
+                let buffer = Buffer.alloc(0);
+                readStream.on("error", (error: any) => {
+                    reject(error);
+                });
+                readStream.on("data", (data: any) => {
+                    buffer = Buffer.concat([buffer, data]);
+                });
+                readStream.on("finish", () => {
+                    resolve(buffer);
+                });
+                readStream.read();
+            }
+        )
+
+    }
 }
