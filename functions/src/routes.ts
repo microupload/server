14d82b0f3ad83ac47dc24842353925ca5b8e0708
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-26 23:09:37 -0500

    Registered FileController's upload method with POST on /upload route

--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -1,12 +1,17 @@
 import { Route } from '@yellow-snow/core';
 import { HttpRoute } from '@yellow-snow/http';
+import * as multer from "multer";
+
+const upload = multer();
 
 // controllers
 
 import { PingController } from './controllers/ping.controller';
+import { FileController } from './controllers/file.controller';
 
 // routes
 
 export const routes: Route<any>[] = [
-    new HttpRoute("/ping","all",PingController,"ping")
+    new HttpRoute("/ping","all",PingController,"ping"),
+    new HttpRoute("/upload","post",FileController,"upload")
 ];
