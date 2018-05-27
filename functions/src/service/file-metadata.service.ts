7d12c1d1e29b855426af8ddeca65c11e65e9ae86
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-26 23:55:23 -0500

    Created a FileMetadataService with a save method

---
+++ functions/src/service/file-metadata.service.ts
@@ -0,0 +1,16 @@
+import * as firebase from "firebase-admin"
+import { FileMetadata } from "../models/file-metadata";
+import { Firebase } from "../models/firebase";
+import { Resolve } from "tsnode-di";
+
+export class FileMetadataService {
+    @Resolve(Firebase)
+    private firebase: Firebase;
+    private firestore: firebase.firestore.Firestore;
+    constructor() {
+        this.firestore = this.firebase.app.firestore();
+    }
+    public async save(metadata: FileMetadata) {
+        await this.firestore.collection("file_metadata").doc(metadata.id).set(metadata);
+    }
+}
